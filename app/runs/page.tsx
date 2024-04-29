5a594ed7019a2933e26782939403e584bbef3046
Anthony Riley <anthonyriley@stanford.edu>
2024-04-29 15:05:05 -0700

    Improved traditions on Runs page

--- app/runs/page.tsx
+++ app/runs/page.tsx
@@ -47,13 +47,16 @@ export default function Contact() {
                 <div className="w-full grid lg:grid-cols-3 gap-4">
                     {
                         copy.runs.traditions.map((tradition, index) => (
-                            <div key={index} className="flex flex-col items-center justify-center border rounded p-4">
-                                <h3 className="text-2xl font-black mt-4">{tradition.name}</h3>
-                                <p className="text-sm text-neutral-500 text-center px-2 leading-4 my-2">
-                                    {tradition.description}
-                                </p>
-                                <div className="my-2 py-2 px-4 rounded bg-rose-100 font-bold text-rose-900 uppercase text-xs">
-                                    {tradition.season}
+                            <div key={index} className="border rounded">
+                                <img src={tradition.image} alt={tradition.name} className="object-cover aspect-video" />
+                                <div className="flex flex-col items-center justify-center p-4 gap-2">
+                                    <h3 className="text-2xl font-black mt-4">{tradition.name}</h3>
+                                    <p className="text-sm text-neutral-500 text-center px-2 leading-4 my-2">
+                                        {tradition.description}
+                                    </p>
+                                    <div className="my-2 py-2 px-4 rounded bg-rose-100 font-bold text-rose-900 uppercase text-xs">
+                                        {tradition.season}
+                                    </div>
                                 </div>
                             </div>
                         ))
